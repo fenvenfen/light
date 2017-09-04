521322bdc8fcdfbdb75e4ca1c764b5deb6ae89e9
fenvenfen <volodymer51558@gmail.com>
2017-09-04 17:11:23 +0200

    Моб версия

--- js/main.js
+++ js/main.js
@@ -2,3 +2,29 @@
 $( ".seaAll" ).click(function() {
   $( ".block_cat" ).toggle("normal");
 });
+
+$(document).ready(function(){
+    $(".lm-navigation li a:first").addClass("first");
+
+    $('.lm-navigation .first').prepend("<span class='image-link catalog'></span>");
+
+    $('.lm-navigation li:nth-child(2) a').prepend("<span class='image-link about'></span>");
+
+
+  $(".mobile-menu li a:first").addClass("first");
+
+    $('.mobile-menu .first').prepend("<span class='image-link catalog'></span>");
+
+    $('.mobile-menu li:nth-child(2) a').prepend("<span class='image-link about'></span>");
+
+        $('.menu-button').click(function() {
+        if ($('.menu-wrapper').hasClass('opened')) {
+            $('.menu-wrapper').removeClass('opened');
+            $(this).removeClass('opened');
+            return;
+        }
+        $('.menu-wrapper').addClass('opened');
+        $(this).addClass('opened');
+    });
+
+})
